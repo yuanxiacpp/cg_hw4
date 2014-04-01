fa15c8eb10e91add36bdfef1032643a3fd413fcc
Yuan Xia <yuan.xia@yale.edu>
2014-03-31 21:52:53 -0400

    1st stage checkin for problem1a

---
+++ problem1/matrix.cpp
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+
+void transpose(double *a, int n) {
+  int i, j;
+  for (i = 0; i < n; ++i) {
+    for (j = i + 1; j < n; ++j) {
+      double tmp = a[i*n + j];
+      a[i*n + j] = a[j*n + i];
+      a[j*n + i] = tmp;
+    }
+  }
+  return;
+}
+
+double* multiply(double *a, double *b, int p, int q, int r) {
+  double *result = (double*)malloc(p*r*sizeof(double));
+  int i, j, k;
+  for (i = 0; i < p; i++) {
+    for (j = 0; j < r; j++) {
+      double sum = 0;
+      for (k = 0; k < q; k++)
+        sum += a[i*q+k] * b[k*r+j];
+      result[i*r+j] = sum;
+    }
+  }
+  return result;
+}
+
+void normalizeCol(double *a, double *g, int n, int col) {
+  double sum = 0;
+  int i;
+  for (i = 0; i < n; i++) {
+    sum += a[n*i+col] * a[n*i+col];
+  }
+  sum = sqrt(sum);
+
+  if (sum == 0)
+    return;
+  for (i = 0; i < n; i++) {
+    a[n*i+col] /= sum;
+    g[n*i+col] /= sum;
+  }
+  return;
+}
+double dotProductForCols(double *a, int n, int col1, int col2) {
+  int i;
+  double result = 0.0;
+  for (i = 0; i < n; i++)
+    result += a[n*i+col1]*a[n*i+col2];
+  return result;
+}
+void updateFromPrevCols(double *u, double *g, int n, int col) {
+  int i, j;
+  double* tmp_u = (double *)malloc(n*sizeof(double));
+  for (i = 0; i < n; i++)
+    tmp_u[i] = 0.0;
+
+  
+  double* tmp_g = (double *)malloc(n*sizeof(double));
+  memcpy(tmp_g, tmp_u, n*sizeof(double));
+  
+  //sum of all prev dot products
+  for (i = 0; i < col; i++) {
+    double dotu = dotProductForCols(u, n, col, i);
+    //double dotg = dotProductForCols(g, n, col, i);
+    
+    /*
+    double dotg = 0.0;
+    int k;
+    for (k = 0; k < n; k++)
+      dotg += u[n*k+i] * g[n*k+col];
+    */
+      
+
+    for (j = 0; j < n; j++) {
+      tmp_u[j] += dotu * u[j*n+i];
+      tmp_g[j] += dotu * g[j*n+i];
+    }
+  }
+
+  //update current col
+  for (i = 0; i < n; i++) {
+    u[i*n+col] -= tmp_u[i];
+    g[i*n+col] -= tmp_g[i];
+  }
+  
+  
+  return;
+}
+
+
+void updateFollowingCols(double *u, double *g, int n, int col) {
+  int i, j;
+  for (j = col; j < n; j++) {
+    double dotu = dotProductForCols(u, n, j, col-1);
+    
+
+    //double dotg = dotProductForCols(g, n, j, col-1);
+    
+    /*
+    double dotg = 0.0;
+    int k;
+    for (k = 0; k < n; k++)
+      dotg += u[n*k+j] * g[n*k+col-1];
+    */
+    
+    //update j-th col with (col-1)-th unit vector
+    for (i = 0; i < n; i++) {
+      u[i*n+j] -= dotu * u[i*n+col-1];
+      g[i*n+j] -= dotu * g[i*n+col-1];
+    }
+  }
+
+  return;
+}
+
+
+void assignIdentity(double *a, int n) {
+  int r, c;
+  for (r = 0; r < n; ++r) {
+    for (c = 0; c < n; ++c) {
+      if (r == c)
+	a[r*n+c] = 1;
+      else
+	a[r*n+c] = 0;
+    }
+  }
+  return;
+}
+
+void inv_double_gs(double *a, int n, double *u, double *b) {
+  
+  int i, j;
+  
+  double* g = (double *)malloc(n*n*sizeof(double));
+  assignIdentity(g, n);
+
+  memcpy(u, a, n*n*sizeof(double));
+
+  //double GS
+  for (i = 0; i < n; i++) {
+    updateFromPrevCols(u, g, n, i);
+    normalizeCol(u, g, n, i);
+    updateFollowingCols(u, g, n, i+1);
+  }
+
+
+  
+  //printf("A: \n");
+  //printMatrix(a, n);
+
+  //printf("U: \n");
+  //printMatrix(u, n);
+
+  double* ut = (double*)malloc(n*n*sizeof(double));
+  memcpy(ut, u, n*n*sizeof(double));
+
+  //transpose(ut, n);
+
+  //printf("Verify U: \n");
+  //printMatrix(multiply(u, ut, n, n, n), n);
+
+  //printf("G: \n");
+  //printMatrix(g, n);
+
+  memcpy(b, multiply(g, ut, n, n, n), n*n*sizeof(double));
+  
+  //printf("A Inverse - B: \n");
+  //printMatrix(b, n);  
+  
+  //printf("Verify Result: \n");
+  //printMatrix(multiply(a, b, n, n, n), n);
+  
+  return;
+}
+/*void problem1(int n) {
+  srand(time(NULL));
+  
+  int i, j;
+
+  double *a = (double *)malloc(n*n*sizeof(double));
+  double *u = (double *)malloc(n*n*sizeof(double));
+  double *b = (double *)malloc(n*n*sizeof(double));
+
+  for (i = 0; i < n; i++)
+    for (j = 0; j < n; j++)
+      a[i*n+j] = (double)rand()/(double)RAND_MAX * 10;
+      //a[i*n+j] = rand() % 10;
+
+  //printMatrix(a, n);
+  
+  
+  inv_double_gs(a, n, u, b);
+
+}
+
+int main() {
+  problem1(5);
+  return 0;
+}
+*/
--- problem1/matt.cpp
+++ problem1/matt.cpp
@@ -5,6 +5,12 @@ using namespace std;
 #include <math.h>
 
 #include "figio.h"
+#include "matrix.h"
+
+const unsigned int max_h = 10000;
+const unsigned int max_w = 10000;
+
+unsigned char* readTiff(const char *filename, int &w, int &h);
 
 
 int main(int argc, char* argv[]) {
@@ -14,6 +20,79 @@ int main(int argc, char* argv[]) {
 	 << endl;
     return 1;
   }
+  int w = max_w;
+  int h = max_h;
+
+  unsigned char* k1 = readTiff(argv[1], w, h);
+  unsigned char* k2 = readTiff(argv[2], w, h);
+  unsigned char* c1 = readTiff(argv[3], w, h);
+  unsigned char* c2 = readTiff(argv[4], w, h);
+
+
+  for (int j = 0; j < h; j++) 
+	for (int i = 0; i < w; i++) {
+	  int index = 4*(j*w+i);
+
+	  int rk1 = k1[index];
+	  int gk1 = k1[index+1];
+	  int bk1 = k1[index+2];
+	 
+	  int rk2 = k2[index];
+	  int gk2 = k2[index+1];
+	  int bk2 = k2[index+2];
+
+	  int delta_r1 = c1[index] - k1[index];
+	  int delta_g1 = c1[index+1] - k1[index+1];
+	  int delta_b1 = c1[index+2] - k1[index+2];
+	  
+	  int delta_r2 = c2[index] - k2[index];
+	  int delta_g2 = c2[index+1] - k2[index+1];
+	  int delta_b2 = c2[index+2] - k2[index+2];
+	  
+
+	  double mat[16] = {0};
+	  mat[0] = 2;
+	  mat[3] = -(rk1 + rk2);
+	  mat[5] = 2;
+	  mat[7] = -(gk1 + gk2);
+	  mat[10] = 2;
+	  mat[11] = -(bk1 + bk2);
+	  mat[12] = -(rk1 + rk2);
+	  mat[13] = -(gk1 + gk2);
+	  mat[14] = -(bk1 + bk2);
+	  mat[15] = rk1*rk1 + gk1*gk1 + bk1*bk1 + rk2*rk2 + gk2*gk2 + bk2*bk2;	  
+
+
+	  double mat_inv[16] = {0};
+	  double u[16] = {0};
+
+	  inv_double_gs(mat, 4, u, mat_inv); 
+
+
+
+
+	  int left[4] = {0};
+	  left[0] = delta_r1 + delta_r2;
+	  left[1] = delta_g1 + delta_g2;
+	  left[2] = delta_b1 + delta_b2;
+	  left[3] = -(rk1 * delta_r1 + gk1*delta_g1 + bk1*delta_b1 + rk2*delta_r2 + gk2*delta_g2 + bk2*delta_b2);
+	  
+	}
+
+
+  
+
 
   return 0;
 }
+
+
+unsigned char* readTiff(const char* filename, int &w, int &h) {
+  unsigned char* im = new unsigned char[max_w*max_h*3];
+  if (!load_tiff_with_alpha(filename, im, w, h)) {
+	cout << "Could not read input file: " << filename << endl;
+	return NULL;
+  }
+  return im;
+}
+
