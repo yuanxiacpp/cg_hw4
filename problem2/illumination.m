8a6589fb8a01bc3cfc83744c2c63628049e71fdc
Yuan Xia <yuan.xia.cpp@gmail.com>
2014-04-03 00:44:07 -0400

    generate problem2a results

--- problem2/illumination.m
+++ problem2/illumination.m
@@ -1,4 +1,4 @@
-function [ direct, scatterd, diff, dir, sca] = illumination(full, pattern1, pattern2)
+function [ direct, scatterd] = illumination(full, pattern1, pattern2)
 %ILLUMINATION Summary of this function goes here
 %   Detailed explanation goes here
     [x, y, d] =size(full);
@@ -21,51 +21,6 @@ function [ direct, scatterd, diff, dir, sca] = illumination(full, pattern1, patt
             end
         end
     end
-    
-    pattern1 = double(pattern1);
-    pattern2 = double(pattern2);
-    diff = pattern1-pattern2;
-	
-	dir = direct;
-	sca = scatterd;    
-
-
-    up = 100;
-    down = 50;
-
-    distance = 5;
-    
-    for i=1:1:x
-         for j=1:1:y
-             for k=1:1:d
-                 
-                 if (abs(diff(i,j,k)) > down && abs(diff(i,j,k)) < up)
-                     diff(i,j,k) = 255;
-                     left = max(1, i-distance);
-                     ceil = max(1, j-distance);
-                     right = min(i+distance, y);
-                     floor = min(j+distance, y);
-                     
-					 count = 0;
-					 sum_d = 0;
-					 sum_s = 0;
-					 for a=left:1:right
-						for b=ceil:1:floor
-							 sum_d = sum_d + direct(a,b,k);
-						     sum_s = sum_s + scatterd(a,b,k);
-							 count = count + 1;                       
-						end
-                     end
-
-					 dir(i,j,k) = sum_d/count;
-					 sca(i,j,k) = sum_s/count;
-                 else
-                     diff(i,j,k)=0;
-                 end
-             end
-         end
-     end
-    diff = uint8(diff);
-
+   
 end
 
--- problem2/test.m
+++ problem2/test.m
@@ -1,4 +1,4 @@
-full=imread('diana/fulllight.tif');
-pattern1=imread('diana/pattern1.tif');
-pattern2=imread('diana/pattern2.tif');
+full=imread('glass/fulllight.tif');
+pattern1=imread('glass/pattern1.tif');
+pattern2=imread('glass/pattern2.tif');
 [direct, scatterd, diff]=illumination(full,pattern1,pattern2);
