1abbc4af77a6e65549e24a12c3deb92ee5193a2b
Yuan Xia <yuan.xia.cpp@gmail.com>
2014-04-03 01:16:09 -0400

    finish problem2c

--- problem2/test.m
+++ problem2/test.m
@@ -1,4 +1,4 @@
 full=imread('glass/fulllight.tif');
 pattern1=imread('glass/pattern1.tif');
 pattern2=imread('glass/pattern2.tif');
-[direct, scatterd, diff]=illumination(full,pattern1,pattern2);
+[direct, scatterd]=illumination_fixed(full,pattern1,pattern2);
